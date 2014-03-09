62d6f58de9aabf8cf0afb57120ed675f9bbc775f
s0833005 <s0833005@monmouth.edu>
2014-03-09 15:51:48 -0400

    Updated Database, Signup, Signin and Signout PHP
    Updated the datbase (Product.Rating is now an integer, Review.reviewdate
    typo fixed). Updated Todo list. Created Signup.php, Signin.php and
    Signout.php. NOTE: ALL THE LINKS NEEDS TO CHANGE TO .PHP NOT .HTML BUT
    CANNOT DO A GLOBAL REPLACE BECAUSE SOME ARE REFERENCES OUTSIDE OUR SITE!

---
+++ BkZ/signout.php
@@ -0,0 +1,11 @@
+﻿<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php
+   session_destroy();
+   echo "You have successfully logged out!<br>";
+?>
+<a href="http://localhost/bzk/index.php">Back to Home Page</a> 
+</body>
+</html>
