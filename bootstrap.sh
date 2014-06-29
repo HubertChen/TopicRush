524498efa716a5be26daba056e2374b0d03936ba
Hubert Chen <HubertChen21@gmail.com>
2014-06-29 19:09:42 -0400

    Added some constants and changes to the initial dev. envir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+sudo apt-get -y update
+# sudo apt-get install -y python-software-properties
+# sudo add-apt-repository -y ppa:ondrej/php5
+# sudo apt-get -y update
 sudo apt-get install -y apache2
 sudo apt-get install -y php5
 sudo apt-get install -y libapache2-mod-php5
@@ -9,5 +12,6 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 123'
 sudo apt-get install -y mysql-server
 sudo apt-get install -y php5-mysql
+# sudo apt-get -y dist-upgrade
 sudo rm -rf /var/www
 sudo ln -fs /vagrant/src /var/www
--- config/defined_constants.php
+++ config/defined_constants.php
@@ -7,9 +7,11 @@
 
 define('ROOT_DIR', 	"/vagrant/");
 define('SRC_DIR', 	ROOT_DIR . "src/");
+define('MODEL_DIR',	SRC_DIR . "models/");
 define('CON_DIR',	ROOT_DIR . "config/");
 define('CON_TEST_DIR',	CON_DIR . "test_env/");
 define('LOG_DIR',	ROOT_DIR . "logs/");
+define('SRC_LIB_DIR',	SRC_DIR . "lib/");
 
 /*
  * Database configuration 
@@ -21,6 +23,14 @@ define('DB_HOST', "localhost");
 define('DB_USER', "root");
 define('DB_PASS', "123");
 define('DB_NAME', "test_db");
+
+/*
+ * Model classes/files
+ */
+define('MODEL_ERROR', 	MODEL_DIR . "Error.php");
+define('MODEL_DB',	MODEL_DIR . "Database.php");
+define('MODEL_USER', 	MODEL_DIR . "User.php");
+
 
 /*
  * Test environment configuration
@@ -38,7 +48,15 @@ define('USER_PASSWORDS',	CON_TEST_DIR . "user_passwords.txt");
  * File names of different files
  */
 define('LOG_ERRORS',		LOG_DIR . "errors.txt");
+define('LOG_DATABASE',		LOG_DIR . "database.txt");
 define('LOG_USER_LOGIN', 	LOG_DIR . "user_login.txt");
 define('LOG_USER_LOGOUT',	LOG_DIR . "user_logout.txt");
 define('LOG_USER_REGISTRATION', LOG_DIR . "user_registration.txt");
+
+/* 
+ * External Libraries
+ * 
+ * Absolute paths for the different libraries
+ */
+define('SRC_LIB_PASSWORD', SRC_LIB_DIR . "password_compat/lib/password.php");
 ?>
