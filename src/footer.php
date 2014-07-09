e204bbb194b0e4e79b6926fb7fbb15ba0751a954
Hubert Chen <HubertChen21@gmail.com>
2014-07-09 14:47:37 -0400

    Added footer

---
+++ src/footer.php
@@ -0,0 +1,11 @@
+<footer>
+	<div id="footer">
+		<!-- Browsers interpret new line as a space; therefore, links placed on line -->
+		<a href="index.php" id="link">HOME</a>/<a href="#communities" id="link">COMMUNITIES</a>/<a href="#contact" id="link">CONTACT</a>
+	</div>
+
+	<div id="copyright">
+		TOPICRUSH.COM 2014
+	</div>
+</footer>
+
