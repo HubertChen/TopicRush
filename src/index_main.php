704b25899adfca33f78403fa9d9c3e5d67a1d2cf
Hubert Chen <HubertChen21@gmail.com>
2014-07-17 17:35:08 -0400

    Added AJAX loading of content

--- src/index_main.php
+++ src/index_main.php
@@ -2,14 +2,14 @@
 	<h3>DISPLAYING ARTICLES FROM COMMUNITIES ABOVE</h3> 	
 </section>
 
-<div class="article">
+<div id="articles">
 		<?php
 			include('models/Database.php');
 			$database = new Database();
 			
 			$articles = $database->query("SELECT * FROM content LIMIT 6;");
 
-			for($count = 0; $count < sizeof($articles); $count++){
+			for($count = 0; $count < 6; $count++){
 				$title 		= $articles[$count]['message'];
 				$owner_id 	= $articles[$count]['ownerid'];
 				$article_id 	= $articles[$count]['articleid'];
@@ -27,9 +27,30 @@
 				<h4 id='communityOverlay' class='overlay'> <span>". $community[0]['name'] ."</span> </h4>
 				</article>";
 			}
+
+			echo "<br>";
 		?>
-		<div id='loadMore'>
-			<a class="btn1" href="#hubert" >LOAD MORE</a>
-		</div>
 </div>
 
+<div id='load'>
+	<a class="btn1" id="loadmore">LOAD MORE</a>
+</div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+	$(document).ready(function(){
+		var count = 6;
+		$("#loadmore").click(function(){
+			$.ajax({
+				type: "GET",
+				url: "index_ajax.php?count=" + count,
+				async: false,
+				success: function(text){
+					response = text;	
+				}
+			});
+			$("#articles").append(response);
+			count += 3;
+		});
+	});
+</script>
