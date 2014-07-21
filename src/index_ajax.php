5f31b9ae851859b675562603b070864f3bab9b8c
Hubert Chen <HubertChen21@gmail.com>
2014-07-21 08:16:16 -0400

    Added missing AJAX file

---
+++ src/index_ajax.php
@@ -0,0 +1,26 @@
+<?php
+include('models/Database.php');
+$database = new Database();
+$count = $_GET['count'];
+
+$articles = $database->query("SELECT * FROM content LIMIT " . $count . ", 6;");
+
+for($count = 0; $count < 6; $count++){
+        $title          = $articles[$count]['message'];
+        $owner_id       = $articles[$count]['ownerid'];
+        $article_id     = $articles[$count]['articleid'];
+
+
+        $author         = $database->query("SELECT username from member where memberid = '$owner_id';");
+        $community      = $database->query("SELECT name from article where articleid = $article_id;");
+
+        if($count % 3 == 0)
+                echo "<br>";
+        echo "<article> 
+        <img src='img/photo.png' alt='temp' class='articleImages'>
+        <h4 id='titleOverlay' class='overlay'> <span>". $title ."</span> </h4>
+        <h4 id='authorOverlay' class='overlay'> <span>". $author[0]['username'] ."</span> </h4>
+        <h4 id='communityOverlay' class='overlay'> <span>". $community[0]['name'] ."</span> </h4>
+        </article>";
+}
+?>
